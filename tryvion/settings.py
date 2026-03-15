67614edf6116d1adaeba0829cecc66353e621463
HIMASREEK <himasreekarunakaran2812@gmail.com>
2026-03-15 22:49:26 +0530

    authentication

--- tryvion/settings.py
+++ tryvion/settings.py
@@ -129,3 +129,16 @@ STATICFILES_DIRS = [
 #media files configuration
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'media'
+
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.INFO: "danger",
+}
+
+# SMPT CONFIGURATION 
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'himasreek557@gmail.com'
+EMAIL_HOST_PASSWORD = 'tzjfrxcmafbtmccb'
+EMAIL_USE_TLS = True
